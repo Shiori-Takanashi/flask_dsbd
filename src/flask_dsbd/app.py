9c68af70ba5f4942a5d4ef835cb9a53dc821c7e4
Takanashi <nao69alter@gmail.com>
2026-06-26 14:39:00 +0900

    refactor: Remove connect.py and Move function to app.py

--- src/flask_dsbd/app.py
+++ src/flask_dsbd/app.py
@@ -1,9 +1,36 @@
+import os
+
+import psycopg
+from dotenv import find_dotenv, load_dotenv
+
+
 def hello() -> str:
     return "Hello, World!"
 
 
+def connect() -> None:
+    load_dotenv(find_dotenv(".env.local"))
+    dbname = os.getenv("POSTGRES_DB")
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+
+    try:
+        with psycopg.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port,
+        ):
+            print("データベースへの接続が成功")
+    except Exception as e:
+        print(f"エラー発生: {e}")
+
+
 def main() -> None:
-    print(hello())
+    connect()
 
 
 if __name__ == "__main__":
